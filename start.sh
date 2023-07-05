4b919a03d95aab3cc69ce2ddba167f17cb9e7b1c
HysunHe <hysunhe@foxmail.com>
2023-07-05 10:28:13 +0800

    setup

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
+which conda > /dev/null 2>&1 || (wget -nc https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && bash Miniconda3-latest-Linux-x86_64.sh -b && eval "$(~/miniconda3/bin/conda shell.bash hook)" && conda init && conda config --set auto_activate_base true);
+pip install -r requirements-dev.txt
+
 pid=`ps -ef | grep -i "oss_append" | grep -v grep | awk '{print $2}'`
 if [[ ! -z "$pid" ]]
 then
