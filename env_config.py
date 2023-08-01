44a7ef80274e2136d387b91c28af4bd5961fb923
HysunHe <hysunhe@foxmail.com>
2023-08-01 13:08:30 +0800

    type cast

--- env_config.py
+++ env_config.py
@@ -6,13 +6,13 @@ OCI_CONFIG_FILE = os.environ.get('OCI_CONFIG_FILE', '~/.oci/config')
 
 WORK_DIR = os.environ.get('BUF_FILE_DIR', '/var/tmp/ossappend')
 
-MAX_QUEUE_SIZE = os.environ.get('MAX_QUEUE_SIZE', int(0))
+MAX_QUEUE_SIZE = int(os.environ.get('MAX_QUEUE_SIZE', 0))
 
 OSS_BUCKET = os.environ.get('OSS_BUCKET', None)
 
-NO_UPDATE_TIMEOUT_SECONDS = os.environ.get('NO_UPDATE_TIMEOUT_SECONDS',
-                                           int(30))
+NO_UPDATE_TIMEOUT_SECONDS = int(os.environ.get('NO_UPDATE_TIMEOUT_SECONDS',
+                                               30))
 
 SERVER_HOST = os.environ.get('SERVER_HOST', '0.0.0.0')
 
-SERVER_LISTEN_PORT = os.environ.get('SERVER_LISTEN_PORT', int(5000))
+SERVER_LISTEN_PORT = int(os.environ.get('SERVER_LISTEN_PORT', 5000))
